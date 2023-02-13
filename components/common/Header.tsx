2aadcebabcbbcfbd8f530820df383aa396ef6a5d
LeeGeonHu <lgh3806@naver.com>
2023-02-13 17:14:54 +0900

    header 구현

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from '../../styles/header.module.scss';
+import Image from 'next/image';
 
-interface Props {}
+interface Props {
+  rightElements?: React.ReactElement[];
+}
 
-const Header = ({}: Props) => {
+const Header = ({ rightElements }: Props) => {
   return (
     <header className={styles.header}>
       <div className={styles.flexItem}>
         <Link href="/" className={styles.box}>
-          <img src="/logo.png" width={140} height={35} alt="로고" />
+          <Image src="/logo.png" width={140} height={35} alt="로고" />
         </Link>
       </div>
+      {rightElements && (
+        <div className={styles.flexItem}> {rightElements} </div>
+      )}
     </header>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
 import Header from '@/components/common/Header';
+import Link from 'next/link';
+import styles from '../styles/header.module.scss';
+import { AiOutlineShareAlt } from 'react-icons/ai';
+import { VscFeedback } from 'react-icons/vsc';
 
 export default function Home() {
   return (
     <>
-      <Header></Header>
+      <Header
+        rightElements={[
+          <button
+            onClick={() => {
+              alert('복사');
+            }}
+            className={styles.box}
+            style={{ marginRight: 8 }}
+            key="button"
+          >
+            <AiOutlineShareAlt size={20} />
+          </button>,
+          <Link href="/feedback" key="link" className={styles.box} passHref>
+            <VscFeedback size={20} />
+          </Link>,
+        ]}
+      ></Header>
     </>
   );
 }
