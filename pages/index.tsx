37a3e9ce02612a5574d02a1a5cd22e8fac0125d6
LeeGeonHu <lgh3806@naver.com>
2023-02-16 13:58:15 +0900

    환경변수 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,6 +46,9 @@ export default Home;
 
 export async function getStaticProps() {
   const stores = (await import('../public/stores.json')).default;
+  // const stores = await fetch(
+  //   `${process.env.NEXT_PUBLIC_API_URL}/api/stores`
+  // ).then((res) => res.json());
 
   return {
     props: { stores },
